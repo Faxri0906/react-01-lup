3c5fcf27cf3688419cd9305dea3fa56f419396c6
Fakhri Mammedzade <safm11060917@gmail.com>
2024-07-06 22:20:47 +0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import './App.css';
+import Footer  from './components/Footer';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
       <Navbar/>
-      <h1>Salam</h1>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
